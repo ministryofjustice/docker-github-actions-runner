d51a692b8734d4b170258b14d7c047dc6ddbcfb5
Dan Webb <dan.webb@damacus.io>
2020-12-07 14:58:30 +0000

    More unsafe perms
     ¯\_(ツ)_/¯

--- installers/nodejs.sh
+++ installers/nodejs.sh
@@ -8,8 +8,8 @@
 curl -sL https://raw.githubusercontent.com/mklement0/n-install/stable/bin/n-install | bash -s -- -ny -
 ~/n/bin/n lts
 
-npm install -g grunt gulp n parcel-bundler typescript newman
-npm install -g --save-dev webpack webpack-cli
+npm install --unsafe-perm -g grunt gulp n parcel-bundler typescript newman
+npm install --unsafe-perm -g --save-dev webpack webpack-cli
 npm install --unsafe-perm -g npm
 rm -rf ~/n
 
